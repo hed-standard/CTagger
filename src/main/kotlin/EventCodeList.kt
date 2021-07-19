a91c235d395ba030dbdfcae0104d3b5316635d8f
nucleuscub <ram033@eng.ucsd.edu>
2021-07-19 01:22:00 -0700

    allow selection using up and down arrow keys

--- src/main/kotlin/EventCodeList.kt
+++ src/main/kotlin/EventCodeList.kt
@@ -1,5 +1,5 @@
-import java.awt.Dimension
-import java.awt.Rectangle
+import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.*
@@ -16,33 +16,53 @@ class EventCodeList(val tagger: CTagger) : JList<String>() {
 
     init {
         model = listModel
-        addMouseListener(EventCodeListListener(tagger))
+        addMouseListener(EventCodeListMouseListener(this))
+        addKeyListener(EventCodeListKeyListener(this))
     }
 
     private fun updateListModel() {
         if (listModel.isEmpty)
             codeSet.forEach{ listModel.addElement(it) }
     }
-    private class EventCodeListListener(val tagger:CTagger): MouseAdapter() {
+    private class EventCodeListKeyListener(private val parent:EventCodeList): KeyListener {
+        override fun keyTyped(e: KeyEvent?) {
+        }
+
+        override fun keyReleased(e: KeyEvent?) {
+            if (e != null) {
+                if (e.keyCode == KeyEvent.VK_DOWN || e.keyCode == KeyEvent.VK_UP) {
+                    parent.codeSelected(e.source as EventCodeList)
+                }
+            }
+        }
+
+        override fun keyPressed(e: KeyEvent?) {
+        }
+    }
+
+    private class EventCodeListMouseListener(private val parent:EventCodeList): MouseAdapter() {
         override fun mouseClicked(e: MouseEvent?) {
                 if (e != null && e.clickCount == 1) {
-                    val eList = e.source as EventCodeList
-                    if (!eList.valueIsAdjusting) {
-                        val tagger = eList.tagger
-                        // creating new one
-                        val curField = tagger.fieldList.selectedItem.toString()
-                        val selected = eList.selectedValue
-                        println("code $selected selected")
-
-                        // create new HedTagInput pertaining to the curField-curCode pair
-                        val hedTagInput = HedTagInput(tagger, curField, selected)
-
-                        eList.prevSelected = selected
-
-                        // hide search result pane
-                        tagger.hideSearchResultPane()
-                    }
+                    parent.codeSelected(e.source as EventCodeList)
                 }
+        }
+    }
+
+    private fun codeSelected(eList: EventCodeList) {
+        if (!eList.valueIsAdjusting) {
+            val tagger = eList.tagger
+            // creating new one
+            val curField = tagger.fieldList.selectedItem.toString()
+            val selected = eList.selectedValue
+            println("code $selected selected")
+
+            // create new HedTagInput pertaining to the curField-curCode pair
+            val hedTagInput = HedTagInput(tagger, curField, selected)
+
+            eList.prevSelected = selected
+
+            // hide search result pane
+            tagger.hideSearchResultPane()
         }
     }
 }
